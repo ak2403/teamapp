a35c68b07c2e52c272b3c9273a4473b50b91c450
ARUN KUMAR <arunpasupathi24@gmail.com>
2018-04-30 22:03:05 +0530

    Added Text and Button in App

--- App.js
+++ App.js
@@ -1,58 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.jszxc
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+import { Platform, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  inputStyle: {
+    width: '100%'
+  }
 });
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Enter your user name"
+        />
+        <TextInput
+          secureTextEntry={true}
+          style={styles.inputStyle}
+          placeholder="Enter your user name"
+        />
+        <Button
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button" />
+      </View>
+    );
+  }
+}
+
+export default App;
